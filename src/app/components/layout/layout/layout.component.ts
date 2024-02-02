2a389af33a51205d04a1b48ca0b4b54d4b345647
aymane Taoufyq <aymane.taoufyq@jobintech-uir.ma>
2024-02-02 15:28:13 +0100

    intialization project

--- src/app/components/layout/layout/layout.component.ts
+++ src/app/components/layout/layout/layout.component.ts
@@ -1,13 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { NavigationComponent } from '../navigation/navigation.component';
 import { RouterOutlet } from '@angular/router';
-
+import { FooterComponent } from '../footer/footer.component';
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [
-    NavigationComponent, RouterOutlet
-  ],
+  imports: [NavigationComponent, RouterOutlet, FooterComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss'
 })
